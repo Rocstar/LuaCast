65f75d7ee6e9bea0b28f0ffb384e584d78c12fb2
Rocstar <john.east@techie.com>
2014-02-22 15:59:44 -0800

    Update Outer_Ra_Kaznar_Lair_J06.lua

--- Outer_Ra_Kaznar_Lair_J06.lua
+++ Outer_Ra_Kaznar_Lair_J06.lua
@@ -14,11 +14,11 @@ local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
 if(ta.Entity~=nil)then return ta.Entity.Name;else return nil;end end
 
 function c(nMode, text)
-if(string.Contains(text,'has joined the'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
-elseif(string.Contains(text,'is victorious'))then timer.RemoveTimer('s');end end
+if(string.Contains(text,'joined'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
+elseif(string.Contains(text,'victorious'))then timer.RemoveTimer('s');end end
 
 function Outer_Ra_Kaznar_Lair_J06:OnLoad()
-timer.Create("s",3.1,0,s);events.Add("onHandleNewChatLine","plugin_c_event",c);end
+events.Add("onHandleNewChatLine","plugin_c_event",c);end
 
 function Outer_Ra_Kaznar_Lair_J06:OnUnload()
 timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 

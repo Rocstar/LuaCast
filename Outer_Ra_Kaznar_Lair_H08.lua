c65f6e8c3bda18ed9de1db581c3de9169afba56c
Rocstar <john.east@techie.com>
2014-02-22 23:18:48 -0800

    Create Outer_Ra_Kaznar_Lair_H08.lua

---
+++ Outer_Ra_Kaznar_Lair_H08.lua
@@ -0,0 +1,29 @@
+Outer_Ra_Kaznar_Lair_H08={
+Name="Outer Ra'Kaznar Lair H-8",
+Author="Rockstar",
+Description="Auto Cast Stone",
+Version="0.1"};
+
+function s()
+local m={"Dimensional Tether"};for i,v in pairs(m)do if(t()==nil)or(t()~=v)then
+AshitaCore:GetDataModule():SendCommand('/target ' .. 17899806,0);
+elseif(t()==v)then AshitaCore:GetDataModule():SendCommand('/ma Stone <t>',1);end end end
+
+function t()
+local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
+if(ta.Entity~=nil)then return ta.Entity.Name;else return nil;end end
+
+function c(nMode, text)
+if(string.Contains(text,'joined'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
+elseif(string.Contains(text,'victorious'))then timer.RemoveTimer('s');end end
+
+function Outer_Ra_Kaznar_Lair_H08:OnLoad()
+events.Add("onHandleNewChatLine","plugin_c_event",c);end
+
+function Outer_Ra_Kaznar_Lair_H08:OnUnload()
+timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
+
+return Outer_Ra_Kaznar_Lair_H08;
+
+-- In game command to get targeted mob's ID.
+-- /luacast dostring "print(AshitaCore:GetDataModule():GetTarget():GetTargetWindow().Entity.ID);"

65eac1402974ae0fd48cbaccb067618a28b9111a
Rocstar <john.east@techie.com>
2014-02-22 23:20:43 -0800

    Create Marjami_Ravine_Colonization_K08.lua

---
+++ Marjami_Ravine_Colonization_K08.lua
@@ -0,0 +1,30 @@
+Marjami_Ravine_Colonization_K08={
+Name="Marjami Ravine Colonization K-8",
+Author="Rockstar",
+Description="Auto Cast Stone",
+Version="0.1"};
+
+function s()
+local m={"Monolithic Boulder"};for i,v in pairs(m)do if(t()==nil)or(t()~=v)then 
+AshitaCore:GetDataModule():SendCommand( '/target ' .. 17867014, 0);
+AshitaCore:GetDataModule():SendCommand( '/target ' .. 17867015, 0);
+elseif(t()==v)then AshitaCore:GetDataModule():SendCommand('/ma Stone <t>',1);end end end
+
+function t()
+local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
+if(ta.Entity~=nil)then return ta.Entity.Name;else return nil;end end
+
+function c(nMode, text)
+if(string.Contains(text,'joined'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
+elseif(string.Contains(text,'victorious'))then timer.RemoveTimer('s');end end
+
+function Marjami_Ravine_Colonization_K08:OnLoad()
+events.Add("onHandleNewChatLine","plugin_c_event",c);end
+
+function Marjami_Ravine_Colonization_K08:OnUnload()
+timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
+
+return Marjami_Ravine_Colonization_K08;
+
+-- In game command to get targeted mob's ID.
+-- /luacast dostring "print(AshitaCore:GetDataModule():GetTarget():GetTargetWindow().Entity.ID);"

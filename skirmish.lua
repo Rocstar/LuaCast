1bbea580d24b6674e2ebcef9b9bd2286092c1a44
Rocstar <john.east@techie.com>
2014-03-06 02:59:19 -0800

    Update skirmish.lua

--- skirmish.lua
+++ skirmish.lua
@@ -1,27 +1,18 @@
-skirmish={
-Name="mob_list",
-Author="Rockstar",
-Description="Auto Cast Stone",
-Version="0.1"};
+skirmish={Name="skirmish",Author="Rockstar",Description="Auto Cast Stone",Version="0.1"};
 
-function s()
-local m={"Breathless Clansman","Shambling Naraka","Dismal Djinn","Effluvial Acuex","Acrimonious Dullahan",
+function s()local m={"Breathless Clansman","Shambling Naraka","Dismal Djinn","Effluvial Acuex","Acrimonious Dullahan",
 "Kantankerous Yztarg","","","","","","","","","","","","","","","",""};
 for i,v in pairs(m)do if(t()==nil)or(t()~=v)then AshitaCore:GetDataModule():SendCommand('/target <bt>',1);
 elseif(t()==v)then AshitaCore:GetDataModule():SendCommand('/ma Stone <t>',1);end end end
 
-function t()
-local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
+function t()local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
 if(ta.Entity~=nil)then return ta.Entity.Name;else return nil;end end
 
-function c(nMode, text)
-if(string.Contains(text,'skirmish start'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
-elseif(string.Contains(text,'skirmish stop'))then timer.RemoveTimer('s');end end
+function c(nMode, text)if(string.Contains(text,'start casting'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
+elseif(string.Contains(text,'stop casting'))then timer.RemoveTimer('s');end end
 
-function skirmish:OnLoad()
-events.Add("onHandleNewChatLine","plugin_c_event",c);end
+function skirmish:OnLoad()events.Add("onHandleNewChatLine","plugin_c_event",c);end
 
-function skirmish:OnUnload()
-timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
+function skirmish:OnUnload()timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
 
 return skirmish;

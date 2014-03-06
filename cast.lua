d47c804e362d5daf08a18e147db8b6aa35b3b916
Rocstar <john.east@techie.com>
2014-03-06 03:09:16 -0800

    Update and rename skirmish.lua to cast.lua

--- cast.lua
+++ cast.lua
@@ -1,4 +1,4 @@
-skirmish={Name="skirmish",Author="Rockstar",Description="Auto Cast Stone",Version="0.1"};
+cast={Name="cast",Author="Rockstar",Description="Auto Cast Stone",Version="0.1"};
 
 function s()local m={"Breathless Clansman","Shambling Naraka","Dismal Djinn","Effluvial Acuex","Acrimonious Dullahan",
 "Kantankerous Yztarg","","","","","","","","","","","","","","","",""};
@@ -8,11 +8,11 @@ elseif(t()==v)then AshitaCore:GetDataModule():SendCommand('/ma Stone <t>',1);end
 function t()local ta=AshitaCore:GetDataModule():GetTarget():GetTargetWindow();
 if(ta.Entity~=nil)then return ta.Entity.Name;else return nil;end end
 
-function c(nMode, text)if(string.Contains(text,'start casting'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
-elseif(string.Contains(text,'stop casting'))then timer.RemoveTimer('s');end end
+function c(nMode, text)if(string.Contains(text,'start cast'))then timer.RemoveTimer('s');timer.Create("s",3.1,0,s);
+elseif(string.Contains(text,'stop cast'))then timer.RemoveTimer('s');end end
 
-function skirmish:OnLoad()events.Add("onHandleNewChatLine","plugin_c_event",c);end
+function cast:OnLoad()events.Add("onHandleNewChatLine","plugin_c_event",c);end
 
-function skirmish:OnUnload()timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
+function cast:OnUnload()timer.RemoveTimer('s');events.Remove("onHandleNewChatLine","plugin_c_event");end 
 
-return skirmish;
+return cast;
